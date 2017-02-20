555b06ed605f9e61f7201eac90401a9e49e179aa
Fruju <frujujuice@users.noreply.github.com>
2017-02-20 13:57:33 +1000

    Add files via upload

---
+++ killUWPapps.ps1
@@ -0,0 +1,39 @@
+﻿$apps=@(     
+    "9E2F88E3.Twitter"
+    "ClearChannelRadioDigital.iHeartRadio"
+    "Flipboard.Flipboard"
+    "king.com.CandyCrushSodaSaga"
+    "Microsoft.3DBuilder"
+    "Microsoft.Appconnector"
+    "Microsoft.BingFinance"
+    "Microsoft.BingNews"
+    "Microsoft.BingSports"
+    "Microsoft.BingWeather"
+    "Microsoft.CommsPhone"
+    "Microsoft.Getstarted"
+    "Microsoft.Messaging"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.Office.OneNote"
+    "Microsoft.Office.Sway"
+    "Microsoft.People"
+    "Microsoft.SkypeApp"
+    "Microsoft.Windows.Phone"
+    "Microsoft.WindowsAlarms"
+    "Microsoft.WindowsCamera"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsPhone"
+    "Microsoft.XboxApp"
+    "Microsoft.ZuneMusic"
+    "Microsoft.ZuneVideo"
+    "Microsoft.MinecraftUWP"
+    "ShazamEntertainmentLtd.Shazam"        
+)
+
+foreach ($app in $apps) {    
+    Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage
+    Get-AppXProvisionedPackage -Online | where DisplayName -EQ $app | Remove-AppxProvisionedPackage -Online
+            
+    $appPath="$Env:LOCALAPPDATA\Packages\$app*"
+    Remove-Item $appPath -Recurse -Force -ErrorAction 0
+}
